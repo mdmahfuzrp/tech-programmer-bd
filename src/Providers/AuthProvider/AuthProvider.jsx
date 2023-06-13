b30a3edcd44adb13ea47b85b47b0a4742221352d
Mahfuz RP <mahfuzrpsmorg@gmail.com>
2023-06-14 01:08:42 +0600

    theme toggle btn implement

--- src/Providers/AuthProvider/AuthProvider.jsx
+++ src/Providers/AuthProvider/AuthProvider.jsx
@@ -1,25 +1,51 @@
-import { createContext } from "react";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+
     const handleSignup = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
     const handleLogin = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
+
+    const handleLogout = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser);
+            console.log(currentUser);
+            setLoading(false);
+        })
+        return ()=>{
+            return unsubscribe();
+        }
+    },[])
 
 
 
     const userInfo = {
         handleSignup,
         auth,
-        handleLogin
+        handleLogin,
+        handleLogout,
+        user,
+        loading,
     }
     return (
         <AuthContext.Provider value={userInfo}>
--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -1,12 +1,44 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo/techprogrammerbd.png';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Providers/AuthProvider/AuthProvider';
+import { RotatingLines } from 'react-loader-spinner';
 const NavBar = () => {
+    const { handleLogout, user, loading } = useContext(AuthContext);
+    const [toggle, setToggle] = useState(false);
+    const [theme, setTheme] = useState('light-theme');
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    }
+    else {
+      setTheme('light-theme');
+    }
+  }
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme])
+
     const listItems = <>
-    <li><Link>Home</Link></li>
-    <li><Link>Instructors</Link></li>
-    <li><Link>Classes</Link></li>
-    <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link>Home</Link></li>
+        <li><Link>Instructors</Link></li>
+        <li><Link>Classes</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
     </>
+
+    const handleToggleBtn = () => {
+        setToggle(!toggle);
+    }
+
+    const handleUserLogout = () => {
+        handleLogout()
+            .then(() => { })
+            .catch(error => {
+                console.log(error);
+            })
+    }
     return (
         <div className='bg-[var(--bg-color)] shadow-md mb-10'>
             <div className="navbar w-11/12 mx-auto">
@@ -27,7 +59,24 @@ const NavBar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <Link to='/login' className='custom-btn'>Login Here</Link>
+                    <div className='mr-2 flex items-center border bg-[#9833f9] p-2 px-3 rounded-full'>
+                        <label htmlFor="toggleBtn" className='mr-1 text-white'>Theme </label>
+                        <input type="checkbox" id='toggleBtn' className={`toggle ${toggle ? "bg-[#9833f9] border-[#9833f9]" : 'border-[#9833f9] bg-gray-400'}`} onClick={handleToggleBtn} checked={toggle} />
+                    </div>
+                    {
+                        !loading ? <>
+                            {
+                                user ? <Link onClick={handleUserLogout} className='custom-btn'>Logout</Link>
+                                    : <Link to='/login' className='custom-btn'>Login Here</Link>
+                            }
+                        </> : <RotatingLines
+                            strokeColor="#9833f9"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            width="45"
+                            visible={true}
+                        />
+                    }
                 </div>
             </div>
         </div>
